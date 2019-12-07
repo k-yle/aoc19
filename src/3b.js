63e2c799b629833e2ca06d46e057057d41b3d19e
Kyle Hensel <k-yle@users.noreply.github.com>
2019-12-07 16:37:14 +1300

    day 3

--- src/3b.js
+++ src/3b.js
@@ -4,17 +4,18 @@ const [line1, line2] = `${fs.readFileSync("./src/3.txt")}`.split("\n");
 
 const visited = {};
 const coord = { x: 0, y: 0 };
+let steps = 0;
 
 for (const instr of line1.split(",")) {
   const dir = instr[0];
   const distance = Number(instr.substr(1));
   for (let i = 0; i < distance; i++) {
     coord["LR".includes(dir) ? "x" : "y"] += "LD".includes(dir) ? -1 : 1;
-    visited[coord.x + "," + coord.y] = 1;
+    visited[coord.x + "," + coord.y] = ++steps;
   }
 }
 
-coord.x = coord.y = 0;
+coord.x = coord.y = steps = 0;
 
 const dup = [];
 for (const instr of line2.split(",")) {
@@ -22,10 +23,10 @@ for (const instr of line2.split(",")) {
   const distance = Number(instr.substr(1));
   for (let i = 0; i < distance; i++) {
     coord["LR".includes(dir) ? "x" : "y"] += "LD".includes(dir) ? -1 : 1;
-    if (visited[coord.x + "," + coord.y]) dup.push([coord.x, coord.y]);
+    ++steps;
+    if (visited[coord.x + "," + coord.y])
+      dup.push(steps + visited[coord.x + "," + coord.y]);
   }
 }
 
-console.log(
-  dup.map(([x, y]) => Math.abs(x) + Math.abs(y)).sort((a, b) => a - b)[0]
-);
+console.log(dup.sort((a, b) => a - b)[0]);
